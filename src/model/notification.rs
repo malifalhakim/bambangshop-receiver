b0bd2e8b80f10fbd26e0f2b018e9d44dd80e499e
alif <malifalhakim11@gmail.com>
2024-04-15 15:24:13 +0700

    Create Notification database and Notification repository struct skeleton

--- src/model/notification.rs
+++ src/model/notification.rs
@@ -17,7 +17,7 @@ impl Display for Notification{
         if self.status.to_uppercase().eq("CREATED"){
             return write!(f, "Hello {}, let's try our new {} product: {}, only in BambangShop! Check it out: {}",
             self.subscriber_name, self.product_type.to_lowercase(), self.product_title, self.product_url )
-        }else if (self.status.to_uppercase().eq("DELETED")){
+        }else if self.status.to_uppercase().eq("DELETED"){
             return write!(f, "Hello {}, we informed that out {} product called {} already sold out...",
             self.subscriber_name, self.product_type.to_lowercase(), self.product_title);
         }else{
